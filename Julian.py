fb82ec49ec928b43e2dd2a36af282237868be07b
julian ruiz <juliancho_900@hotmail.com>
2016-08-08 20:34:30 -0500

    actualizacion

---
+++ Julian.py
@@ -0,0 +1,86 @@
+
+# coding: utf-8
+
+# In[1]:
+
+print "hola mundo"
+
+
+# $\sum_{i=1}^n{5}$
+
+# \displaystyle\lim_{x \to{+}\infty}{}(x)-\sum_{i=p}^{}-log(2)+\int_x^\infty\frac{dt}{t(t^2-1)log(t)}
+
+# $f(x)=\displaystyle\lim_{x \to{+}\infty}{}(x)-\sum_{i=p}^{}-log(2)+\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$
+
+# In[4]:
+
+2 + 4 * 6 / 8.0 - 3
+
+
+# In[6]:
+
+3/2
+
+
+# In[7]:
+
+3+5j
+
+
+# In[11]:
+
+(3+5j)-(1-1j)
+
+
+# In[13]:
+
+5**3
+
+
+# In[17]:
+
+complex(_13)
+
+
+# In[20]:
+
+float(_6)
+
+
+# In[22]:
+
+round(20)
+
+
+# In[25]:
+
+max(8,10,20,30,-5)
+
+
+# In[27]:
+
+min(1,3,-5)
+
+
+# $a\equiv5$
+
+# In[28]:
+
+x,y,z = 1,2,3
+print(x,y,z)
+
+
+# In[35]:
+
+x=5
+y=1
+if x>y:
+    print "El mayor es",x
+else:
+    print "El mayor es ",y
+
+
+# In[ ]:
+
+
+
